8d61ae5c97cb2b01698d12a07a033cee3790c2b3
kyle-maclachlan-bvc <k.maclachlan882@mybvc.ca>
2026-04-18 13:23:55 -0600

    Push All

--- Assignment2/Assets/Scripts/UI_UX/LevelClear.cs
+++ Assignment2/Assets/Scripts/UI_UX/LevelClear.cs
@@ -5,12 +5,12 @@ public class LevelClear : Singleton<LevelClear>
 {
     [SerializeField] private UIMessageBase ui;
 
-    public void ShowToast(string message)
+    public void ShowLevelClear(string message)
     {
         ui.Show(message);
     }
 
-    public void HideToast()
+    public void HideLevelClear()
     {
         ui.Hide();
     }
